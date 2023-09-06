bfb169cbcb8705cd086ddf7fa098b5eb26a494c0
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-09-05 23:33:53 -0300

    Iniciado proyecto de Next

---
+++ app/tienda/categorias/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage(){
+    return (
+    <div className="Container">
+        <h1>Tienda</h1>
+        <ul>
+            <li>elemento 1</li>
+        </ul>
+    </div>
+    )
+}
