a2d739caa585048d868229679519a8b91e67fc81
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-09-06 00:48:20 -0300

    Añadida metadata :3

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
     url: ';3',
     images: [
       {
-        url: '../public/nyan-cat-kaomoji.gif',
+        url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbubble.com%2Fes%2Fi%2Fpegatina%2Fkaomoji-ascii-gato-varita-m%25C3%25A1gica-brilla-de-fragmentals%2F138147289.EJUG5&psig=AOvVaw3eUdlOaUWnN4Ay2gwgi08Q&ust=1694058312077000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMjZ-5CJlYEDFQAAAAAdAAAAABAR',
         width: 800,
         height: 600,
       }
