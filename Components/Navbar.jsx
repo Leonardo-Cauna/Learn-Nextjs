1198b11e6bcdba056b20126459f67d467e44b441
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-09-07 00:52:28 -0300

    añadidos estilos con tailwind y pagina de post en especifico

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import "./navbar.css"
 
 export default function Navbar() {
-<<<<<<< HEAD:Components/navbar.jsx
   return(
     <nav className='navbar py-5'>
       <Link href={"/"}>
@@ -33,28 +32,3 @@ export default function Navbar() {
     </nav>
   )
 }
-=======
-    return(<nav>
-          <h1>Navbar</h1>
-
-          <ul>
-            <li>
-              <Link href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/tienda/">
-                about
-              </Link>
-            </li>
-            <li>
-              <Link href="/tienda/categorias">
-                Categorias
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        )
-}
->>>>>>> fcdfe265520700e1bc51615f22958ff3f807645a:Components/Navbar.jsx
